55b7eaabebe5181290cff3387651942718c62660
Gamer-49 <gamer_-49@invisionsquare.com>
2025-03-12 23:30:28 -0400

    config update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://gamer-49.github.io/e-plantshopping/",
+  base: "/e-plantshopping/dist/",
   plugins: [react()],
 })
